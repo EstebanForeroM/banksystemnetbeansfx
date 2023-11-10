459e93249c939ef217c64a7dcb61c445ef4e0670
Esteban Forero <estebanfomo@unisabana.edu.co>
2023-11-10 14:48:48 -0500

    Main functionality corrected

--- src/com/finalproject/frameworks/MainFunctionality.java
+++ src/com/finalproject/frameworks/MainFunctionality.java
@@ -1,10 +1,5 @@
 package com.finalproject.frameworks;
 
-import com.finalproject.controllers.AppController;
-import com.finalproject.controllers.ClientRepository;
-import com.finalproject.controllers.ProductRepository;
-import com.finalproject.frameworks.repositoryLogic.TextFileClientRepository;
-import com.finalproject.frameworks.repositoryLogic.TextFileProductRepository;
 import java.io.IOException;
 import java.net.URL;
 import javafx.application.Application;
@@ -17,12 +12,8 @@ import javafx.stage.Stage;
 public class MainFunctionality extends Application {
 
     private static Scene scene;
-    public static AppController appController;
 
     public static void main(String[] args) {
-        ClientRepository clientRepository = new TextFileClientRepository("src\\data\\users\\users.txt");
-        ProductRepository productRepository = new TextFileProductRepository("src\\data\\products");
-        appController = new AppController(clientRepository, productRepository);
         launch(args);
     }
 
